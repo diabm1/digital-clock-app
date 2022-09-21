ea5b4bbee46e8aa150dcfbc17f493e8fd6135d32
diabm1 <diabm1855@gmail.com>
2022-09-21 12:24:04 -0400

    added multiple functionalities and now a bit to how to display the day and add the suffix

--- assets/index.js
+++ assets/index.js
@@ -1,28 +1,122 @@
-// Clock should show the seconds update every one second (in other words it should seem as if the clock is ticking
-
-// Clock should show current time in format HH:MM:SS (Example: 03:39:09)
-
-// Clock should show current time for hours, minutes and seconds with leading zeros.  For example, if it should say 04:04:09 instead of 4:4:9.
-
-// Clock should show current date in format Day, Month Date Year (eg. Monday, August 12th 2021)
-
 function init() {
   time();
   date();
 }
 
+// Clock should show the seconds update every one second (in other words it should seem as if the clock is ticking
 function time() {
-  const date = new Date();
-  const current_time =
-    date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-  document.getElementById("time").innerText = "Time: " + current_time;
+  let date = new Date();
+  let seconds = date.getSeconds();
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  //   const current_time =
+  //     date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+  seconds = leadingZero(seconds);
+  minutes = leadingZero(minutes);
+  hours = leadingZero(hours);
+
+  let amOrPm = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+
+  document.getElementById(
+    "time"
+  ).innerText = `${hours}:${minutes}:${seconds} ${amOrPm}`;
 }
 
+setInterval(time, 1000);
+
+// Clock should show current time in format HH:MM:SS (Example: 03:39:09)
+// Clock should show current time for hours, minutes and seconds with leading zeros.  For example, if it should say 04:04:09 instead of 4:4:9.
+function leadingZero(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
+// Clock should show current date in format Day, Month Date Year (eg. Monday, August 12th 2021)
 function date() {
   const date = new Date();
-  const current_date =
-    date.getDate() + "st, " + (date.getMonth() + 1) + " " + date.getFullYear();
-  document.getElementById("date").innerText = "Date: " + current_date;
+  let day = date.getDay();
+  let today = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+  //   const current_date =
+  //     date.getDate() + ", " + (date.getMonth() + 1) + " " + date.getFullYear();
+
+  switch (day) {
+    case 0:
+      today = "Sunday";
+      break;
+    case 1:
+      today = "Monday";
+      break;
+    case 2:
+      today = "Tuesday";
+      break;
+    case 3:
+      today = "Wednesday";
+      break;
+    case 4:
+      today = "Thursday";
+      break;
+    case 5:
+      today = "Friday";
+      break;
+    case 6:
+      today = "Saturday";
+  }
+
+  switch (month) {
+    case 1:
+      month = "January";
+      break;
+    case 2:
+      month = "February";
+      break;
+    case 3:
+      month = "March";
+      break;
+    case 4:
+      month = "April";
+      break;
+    case 5:
+      month = "May";
+      break;
+    case 6:
+      month = "June";
+      break;
+    case 7:
+      month = "July";
+      break;
+    case 8:
+      month = "August";
+      break;
+    case 9:
+      month = "September";
+      break;
+    case 10:
+      month = "October";
+      break;
+    case 11:
+      month = "November";
+      break;
+    case 12:
+      month = "December";
+      break;
+  }
+
+  document.getElementById(
+    "date"
+  ).innerText = `${today}, ${month} ${day} ${year}`;
+  console.log(`${today}, ${month} ${year}`);
 }
+
+// function addMonthNameToNum(){
+//     if(month == 9){
+//         month = "September"
+//       }
+// }
 
 init();
